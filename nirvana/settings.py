0333f3736d8994b7e62736adb88dcd8516b5b8aa
Saugattubehd <50110013+Saugattubehd@users.noreply.github.com>
2025-06-11 16:34:52 +0545

    First commit

--- nirvana/settings.py
+++ nirvana/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from django.contrib import messages 
 from pathlib import Path
 import os
-
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h-%t&@&u*z!b21(l_k9#0(#a$@0&5sy+vb$j6!4%h#em!k8g3t'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config("DEBUG",cast=bool)
 
-ALLOWED_HOSTS = []
+
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -82,6 +85,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+DATABASES["default"]=dj_database_url.parse(config("DATABASE_URL"))
 AUTH_USER_MODEL = 'userauths.User'  # Custom user model
 
 # Password validation
